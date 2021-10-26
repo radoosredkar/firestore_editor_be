3c792347d9011eaa8303a08195a5f275f7b7ff8f
rado <radoosredkar@hotmail.com>
2021-10-26 18:04:45 +0200

    Add firestore db

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from app import app
 from flask_graphql import GraphQLView
 from application import schemas
 from config import settings
+import application.db_firestore as db
 
 app.add_url_rule(
     "/graphql",
@@ -13,6 +14,16 @@ app.add_url_rule(
 @app.route("/")
 def root():
     return f"App is online ... (environment={settings.environment})"
+
+
+@app.route("/test")
+def test():
+    colls = db.get_collections()
+    coll = db.get_collection("dev_col2")
+    rs = ""
+    for c in colls:
+        rs = rs + str((c.get()[0]))
+    return rs
 
 
 if __name__ == "__main__":
