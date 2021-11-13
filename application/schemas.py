2130c4d0e6d101fa0bd98a33c7140bc3c340a105
rado <radoosredkar@hotmail.com>
2021-11-13 18:47:18 +0100

    Add fields display

--- application/schemas.py
+++ application/schemas.py
@@ -2,63 +2,83 @@ import application.db_firestore as db
 import graphene
 from graphene import ObjectType, Mutation, String, Int, Field, List, Enum
 from app import app
+import google.cloud.firestore_v1.base_document as doc
 
 
 class CollectionsTypeSchema(graphene.Enum):
+    DOCUMENT = "DOCUMENT"
     FIELD = "FIELD"
     COLLECTION = "COLLECTION"
 
 
 class GcpField(ObjectType):
+    parents = String()
     name = String()
     type = CollectionsTypeSchema()
 
-    def __init__(self, name, _type):
+    def __init__(self, name: str, _type: graphene.Enum, parents: str):
         self.name = name
         self.type = _type
+        self.parents = parents
 
     def resolve_name(self, info):
-        return "a"
+        return self.name
 
     def resolve_type(self, info):
-        return "b"
+        return self.type
+
+    def resolve_parents(self, info):
+        return self.parents
+
+
+def show_document_fields(doc):
+    app.logger.info("Call show_document_fields doc.id:%s", doc.id)
+    fields = []
+    fields_dict = doc.to_dict()
+    for collection_ref in doc.reference.collections():
+        app.logger.info(
+            "loop collections  in doc.id=%s collection_ref=%s",
+            doc.id,
+            collection_ref.id,
+        )
+        fields.append(
+            GcpField(collection_ref.id, CollectionsTypeSchema.COLLECTION.value, doc.id)
+        )
+        documents: doc.DocumentSnapshot = collection_ref.get()
+        for subdoc in documents:
+            fields += show_document_fields(subdoc)
+    # Get fields
+    for k, v in fields_dict.items():
+        app.logger.info("loop field_dict  k=%s v=%s", k, v)
+        fields.append(GcpField(k, CollectionsTypeSchema.FIELD.value, doc.id))
+    return fields
 
 
 def show_collection(name: str, depth: int):
     docs = db.get_collection(name)
     fields = []
+    app.logger.info("Call show_collection name:%s depth:%s", name, depth)
     for doc in docs:
-        fields_dict = doc.to_dict()
-        # Get collections
-        for collection_ref in doc.reference.collections():
-            fields.append(
-                GcpField(collection_ref.id, CollectionsTypeSchema.COLLECTION.value)
-            )
-            # pass
-        # Get fields
-        for k, v in fields_dict.items():
-            fields.append(GcpField(k, CollectionsTypeSchema.FIELD.value))
+        app.logger.info("loop docs doc.id=%s", doc.id)
+        fields.append(GcpField(doc.id, CollectionsTypeSchema.DOCUMENT.value, name))
+        fields += show_document_fields(doc)
     return fields
 
 
 def create_schema():
-    fields = Field(GcpField)
-
-    def resolve_fields(self, info):
-        flds = show_collection("dev_col2", 1)
-        return flds[0]
+    fields = List(GcpField)
 
     def make_resolver(rec_name, rec_cls):
         def resolver(self, info):
-            return f"Record is {rec_name}"
+            return show_collection("dev_col1", 1)
 
         resolver.__name__ = f"resolve_{rec_name}"
         return resolver
 
     fields_dict = {}
-    fields_dict["fields"] = graphene.String()
-    fields_dict["resolve_fields"] = resolve_fields
-    fields = show_collection("dev_col1", 1)
+    fields_dict["fields"] = fields
+    fields_dict["resolve_fields"] = make_resolver("fields", fields)
+    # fields = show_collection("dev_col1", 1)
     #    for f in fields:
     #        fields_dict[str(f)] = graphene.String()
     #        fields_dict[f"resolve_{str(f)}"] = make_resolver(str(f), String)
